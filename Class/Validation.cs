262ce9f0faa90ce46d206cac7eac720f963d98ae
Lizzaran <lizzaran92@gmail.com>
2014-08-11 21:11:59 +0200

    Unneeded method

--- Class/Validation.cs
+++ Class/Validation.cs
@@ -2,10 +2,8 @@
 using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Media;
 using SVNCompiler.Data;
 
 /*
@@ -57,41 +55,6 @@ namespace SVNCompiler.Class
             return !string.IsNullOrWhiteSpace(error.ToString())
                 ? new ValidationResult(false, error.ToString())
                 : ValidationResult.ValidResult;
-        }
-
-        public static bool IsValid(DependencyObject parent)
-        {
-            bool valid = true;
-            LocalValueEnumerator localValues = parent.GetLocalValueEnumerator();
-            while (localValues.MoveNext())
-            {
-                LocalValueEntry entry = localValues.Current;
-                if (BindingOperations.IsDataBound(parent, entry.Property))
-                {
-                    Binding binding = BindingOperations.GetBinding(parent, entry.Property);
-                    foreach (ValidationRule rule in binding.ValidationRules)
-                    {
-                        ValidationResult result = rule.Validate(parent.GetValue(entry.Property), null);
-                        if (!result.IsValid)
-                        {
-                            BindingExpression expression = BindingOperations.GetBindingExpression(parent, entry.Property);
-                            Validation.MarkInvalid(expression,
-                                new ValidationError(rule, expression, result.ErrorContent, null));
-                            valid = false;
-                        }
-                    }
-                }
-            }
-            for (int i = 0; i != VisualTreeHelper.GetChildrenCount(parent); ++i)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
-                if (!IsValid(child))
-                {
-                    valid = false;
-                }
-            }
-
-            return valid;
         }
     }
 
